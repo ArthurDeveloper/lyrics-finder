4f2475bc8b554e61485e4eb1b1ab0d49219d4733
ArthurDev <arthurdevoficial@gmail.com>
2023-05-04 20:51:57 -0300

    Now the frontend pulls the lyrics from the server

--- frontend/src/components/SearchForm.vue
+++ frontend/src/components/SearchForm.vue
@@ -11,11 +11,11 @@
 			async search(e: Event) {
 				e.preventDefault();
 
-				const response = await fetch(`https://lyrist.vercel.app/api/${this.title}/${this.author}`);
+				const response = await fetch(`http://localhost:8000/lyrics/${this.title}/${this.author}`);
 				const data = await response.json();
 
 				if (response.status === 200) {
-					this.response = data.lyrics;
+					this.response = data.Lyric.replaceAll('\n', '<br>');
 				} else {
 					this.response = 'No lyrics found';
 				}
@@ -30,6 +30,6 @@
 		<input type="text" placeholder="Title" aria-placeholder="Title" v-model="title" />	
 		<button type="submit" @click="search">Search 🔎</button>
 
-		<h2>{{  response }}</h2>
+		<h2 v-html="response"></h2>
 	</form>
 </template>
