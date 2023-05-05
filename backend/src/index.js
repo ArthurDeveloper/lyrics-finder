c89a5be08402a5f0de529d21fa7bdbb98cbf198d
ArthurDev <arthurdevoficial@gmail.com>
2023-05-04 21:02:14 -0300

    Removed useless column from lyrics.db

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(path.resolve(__dirname, '../lyrics.db'), sqlite3.OPEN_READWRITE);
 
 const app = express();
+app.use(cors());
 
 app.get('/lyrics/:song', (req, res) => {
 	const song = `${req.params.song}%`;
@@ -23,14 +25,12 @@ app.get('/lyrics/:song/:author', (req, res) => {
 	const song = `${req.params.song}%`;
 	const author = `/%${req.params.author.toLowerCase().split(' ').join('-')}%/`;
 	console.log(author);
-	db.all('SELECT * FROM lyrics WHERE SName LIKE ? AND ALink LIKE ? LIMIT 20', [song, author], (err, rows) => {
+	db.get('SELECT * FROM lyrics WHERE SName LIKE ? AND ALink LIKE ? LIMIT 20', [song, author], (err, row) => {
 		if (err) {
 			return console.error(err.message);
 		}
 
-		return res.json({
-			songs: rows
-		});
+		return res.json(row);
 	});
 });
 
